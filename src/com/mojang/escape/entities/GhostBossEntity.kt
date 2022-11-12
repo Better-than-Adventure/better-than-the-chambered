56bf6489be190bff69cda037b9106c2cbcb76f2b
Joe4422 <josephdotbrown@gmail.com>
2022-11-12 14:04:19 +0000

    + kotlinized more entities

---
+++ src/com/mojang/escape/entities/GhostBossEntity.kt
@@ -0,0 +1,57 @@
+package com.mojang.escape.entities
+
+import com.mojang.escape.Art
+import kotlin.math.atan2
+import kotlin.math.sin
+import kotlin.math.sqrt
+
+class GhostBossEntity(x: Double, z: Double): EnemyEntity(x, z, 4 * 8 + 6, Art.getCol(0xFFFF00)) {
+    private var rotatePos = 0.0
+    private var shootDelay = 0
+
+    init {
+        this.health = 10
+        this.flying = true
+    }
+
+    override fun tick() {
+        animTime++
+        sprite.tex = defaultTex + animTime / 10 % 2
+
+        var xd = (level.player.x + sin(rotatePos) * 2) - x
+        var zd = (level.player.z + sin(rotatePos) * 2) - z
+        var dd = xd * xd + zd * zd
+
+        if (dd < 1) {
+            rotatePos += 0.04
+        } else {
+            rotatePos = level.player.rot
+        }
+
+        if (dd < 4 * 4) {
+            dd = sqrt(dd)
+
+            xd /= dd
+            zd /= dd
+
+            xa += xd * 0.006
+            za += zd * 0.006
+
+            if (shootDelay > 0) {
+                shootDelay--
+            } else if (random.nextInt(10) == 0) {
+                shootDelay = 10
+                level.addEntity(Bullet(this, x, z, atan2(level.player.x - x, level.player.z - z), 0.20, 1, defaultColor))
+            }
+        }
+    }
+
+    override fun hurt(xd: Double, zd: Double) {
+        // Do nothing
+    }
+
+    override fun move() {
+        x += xa
+        z += za
+    }
+}
---
+++ src/com/mojang/escape/entities/GhostEntity.kt
@@ -0,0 +1,59 @@
+package com.mojang.escape.entities
+
+import com.mojang.escape.Art
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
+
+class GhostEntity(x: Double, z: Double): EnemyEntity(x, z, 4 * 8 + 6, Art.getCol(0xFFFFFF)) {
+    private var rotatePos = 0.0
+
+    init {
+        this.health = 4
+        this.r = 0.3
+
+        this.flying = true
+    }
+
+    override fun tick() {
+        this.animTime++
+        this.sprite.tex = defaultTex + animTime / 10 % 2
+
+        var xd = (level.player.x + sin(rotatePos)) - x
+        var zd = (level.player.z + cos(rotatePos)) - z
+        var dd = xd * xd + zd + zd
+
+        if (dd < 4 * 4) {
+            if (dd < 1) {
+                this.rotatePos += 0.04
+            } else {
+                this.rotatePos = level.player.rot
+                this.xa += (random.nextDouble() - 0.5) * 0.02
+                this.za += (random.nextDouble() - 0.5) * 0.02
+            }
+
+            dd = sqrt(dd)
+
+            xd /= dd
+            zd /= dd
+
+            xa += xd * 0.004
+            za += zd * 0.004
+        }
+
+        this.move()
+
+        xa *= 0.9
+        za *= 0.9
+    }
+
+    override fun hurt(xd: Double, zd: Double) {
+        // Do nothing
+    }
+
+    override fun move() {
+        x += xa
+        z += za
+    }
+
+}
